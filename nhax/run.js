99e3fa343466e9e769d58e5d50db6b2a251127b6
ScriptItWithCode <62032154+CoreBytee@users.noreply.github.com>
2022-05-24 14:16:20 +0200

    remove stuff

--- nhax/run.js
+++ nhax/run.js
@@ -1,72 +1,28 @@
-
-function loaded() {
-    console.log("run")
-
-    if (document.getElementsByClassName("hstyp1")[0]) {
-        document.getElementsByClassName("hstyp1")[0].remove()
-    }
-
-    TitleBar = document.getElementById("pageHL")
-    HackBar = TitleBar.cloneNode(true)
-    TitleBar.parentElement.appendChild(HackBar)
-
-    HackBar.setAttribute("class", HackBar.getAttribute("class") + " hstyp1")
-    HackBar.innerText = "Theorie"
-    HackBar.setAttribute("style", HackBar.getAttribute("style") + " background-image: url() !important; color: black; background-color: yellow;")
-    //hstyp1
-
-    
-    async function executeaction() {
-        console.log("wadawraw");
-
-        ScriptList = document.getElementsByTagName("head")[0].getElementsByTagName("script")
-        ScriptData = ScriptList[ScriptList.length - 1].innerText;
-
-        ScriptLines = ScriptData.split(";");
-        console.log(ScriptLines)
-
-        for (const Line of ScriptLines) {
-            if (Line.split(".addItem(").length == 2) {
-                SplitLine = Line.split(".addItem(")[1]
-                SplitData = SplitLine.split("\",\"")
-                AnswerName = SplitData[0].split("\"")[1]
-                AnswerData = SplitData[1]
-                //.slice(0, SplitData[1].length - 2).split("|")[0]
-                console.log(AnswerName);
-                console.log(AnswerData);
-
-                console.log(document.getElementById(AnswerName))
-                console.log(document.getElementById(AnswerName).nodeName)
-
-                if (document.getElementById(AnswerName).nodeName == "TEXTAREA") {
-                    
-                } else if (document.getElementById(AnswerName).nodeName == "INPUT") {
-                    document.getElementById(AnswerName).setAttribute("value", AnswerData.split("|")[0])
-                    document.getElementById(AnswerName).dispatchEvent(new Event('input'))
-                }
-
-                
-                
-
-            }
-        };
-
-
-    }
-
-    HackBar.addEventListener("click", executeaction)
+async function testEnabled() {
+    return ((await (await fetch("https://raw.githubusercontent.com/CoreBytee/nhax/main/enabled.json")).json()).enabled != true)
 }
 
+        
 
-function testforload() {
+async function testforload() {
+    if ( testEnabled() ) {
+        console.log("[nhax] Not enabled, exiting...");
+        function sleep(ms) {
+            return new Promise(resolve => setTimeout(resolve, ms));
+        }
+        
+        while (true) {
+            for (let i = 0; i < 360; i++) {
+                document.body.style.setProperty("-webkit-transform", `rotate(${i}deg)`, null);
+                await sleep(2)
+            }
+        }
+    }
+
     if ( document.getElementById("pageHL") ) {
-
         var script = document.createElement('script');
         script.src = chrome.runtime.getURL("inject.js");
         document.head.appendChild(script);
-
-
-        //loaded()
     }
 }
 
